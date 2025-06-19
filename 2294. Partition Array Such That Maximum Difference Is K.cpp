9141e1a7d301673e6980c549ab64e26866bcaa5b
Ibn Omar Romel <ibnomarromel@gmail.com>
2025-06-19 23:09:24 +0600

    Update

---
+++ 2294. Partition Array Such That Maximum Difference Is K.cpp
@@ -0,0 +1,14 @@
+int partitionArray(vector<int>& nums, int k) {
+
+    sort(nums.begin(), nums.end());
+    int n = nums.size(), i = 0, j = 1;
+    int count = 1;
+    while(j < n) {
+        if (nums[j] - nums[i] > k) {
+            count++;
+            i = j;
+        }
+        j++;
+    }
+    return count;
+}
