3ea6b7f1084a61b9863701c0d4c2aafac350c732
Ibn Omar Romel <ibnomarromel@gmail.com>
2025-07-11 01:30:54 +0600

    Update

---
+++ 575. Distribute Candies.cpp
@@ -0,0 +1,13 @@
+int distributeCandies(vector<int>& candyType) {
+    set<int>st;
+    for (auto x : candyType) {
+        st.insert(x);
+    }
+
+    int n = candyType.size() / 2;
+    
+    if (st.size() <= n)
+        return st.size();
+    else
+        return n;
+}
