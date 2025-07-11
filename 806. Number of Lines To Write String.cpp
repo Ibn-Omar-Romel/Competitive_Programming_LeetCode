a08842da66c3b83722c42ff0077c4279363339e5
Ibn Omar Romel <ibnomarromel@gmail.com>
2025-07-11 21:51:54 +0600

    Update

---
+++ 806. Number of Lines To Write String.cpp
@@ -0,0 +1,16 @@
+vector<int> numberOfLines(vector<int>& widths, string s) {
+
+    int count = 1;
+    int total = 0;
+    for (int i = 0; i < s.size(); i++) {
+        int value = widths[s[i] - 'a'];
+
+        if (total + value > 100) {
+            count++;
+            total = value;
+        } else {
+            total += value;
+        }
+    }
+    return {count, total};
+}
