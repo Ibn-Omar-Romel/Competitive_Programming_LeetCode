b8dd2f02ea1ab6e53d682bf7814a411b1e47679b
Ibn Omar Romel <ibnomarromel@gmail.com>
2025-07-18 00:37:59 +0600

    Update

---
+++ 3136. Valid Word.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+bool isValid(string word) {
+
+    if (word.size() < 3)
+        return false;
+
+    bool vowel = false, consotant = false;
+    for (auto x : word) {
+
+        
+        if (isdigit(x)) {
+            continue;
+        }
+
+        x = tolower(x);
+        if (x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u') {
+            vowel = true;
+        }
+        else if (isalpha(x)) {
+            consotant = true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    if (vowel and consotant)    
+        return true;
+    return false;
+}
+
+int main() {
+
+
+    string s;
+    cin >> s;
+
+    
+
+    cout << isValid(s) << endl;
+    return 0;
+}
